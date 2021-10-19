dba57605834a4dbb3c37f64bf3a96b5d857d726c
J4X <angryjayfox@gmail.com>
2021-10-19 19:20:31 +0700

    new testing function was added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 
 app = Flask(__name__)
@@ -10,5 +10,24 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/summ', methods=["POST", "GET"])
+def summ():
+    if request.method == "GET":
+        return render_template("summ.html", c="Введите значение a и b и нажмите \"Вычислить\"")
+
+    if request.method == "POST":
+        a = request.form["a"]
+        b = request.form["b"]
+        c = None
+        try:
+            a, b = int(a), int(b)
+            c = int(a) + int(b)
+        except:
+            c = "a и b должны быть числами"
+        return render_template("summ.html", c=c)
+
+
+
+
 if __name__ == "__main__":
-        app.run(debug=True)
+    app.run(debug=True)
