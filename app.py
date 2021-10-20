06bf26ba2c1028ac0883e0e1b993797529a2fd08
aest <aest@ya.ru>
2021-10-20 21:55:22 +0700

    Gameselection

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gamebase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
+# Работа с базой данных
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gamename = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Article %r' % self.id
+
+
+# Показать главную страницу
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
+
 
 # Вычисление суммы двух чисел
 @app.route('/summ', methods=["POST", "GET"])
@@ -41,6 +56,35 @@ def whatsyourname():
         else:
             hi = "Привет, " + username + "!"
         return render_template("whatsyourname.html", username=username, hi=hi)
+
+
+# Выбор игры
+@app.route('/gameselection', methods=["POST", "GET"])
+def gamelist():
+        articles = Article.query.all()
+        return render_template("gameselection.html", articles=articles)
+
+
+def gameadd():
+    if request.method == "POST":
+        gamename = request.form['gamename']
+
+        article = Article(gamename=gamename)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            text = "Готово"
+            return render_template("gameselection.html", text=text)
+        except:
+            return "При добавлении игры произошла ошибка"
+
+    else:
+        return render_template("gameselection.html")
+
+
+
+
 
 
 if __name__ == "__main__":
